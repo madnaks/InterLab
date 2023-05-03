a9a72cba3457114894dbdd1bf9b1b47e8a527520
MADNAKS\Skander <med.haouam@gmail.com>
2023-05-03 15:30:18 -0400

    Add middleware and apikey in appsettings

---
+++ InterLab-Server/InterLab/InterLab/MiddleWare/PerigonMiddleWare.cs
@@ -0,0 +1,18 @@
+﻿namespace InterLab.MiddleWare
+{
+    public class PerigonMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        private const string APIKEY = "ApiKey";
+
+        public PerigonMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            await _next(httpContext);
+        }
+    }
+}
