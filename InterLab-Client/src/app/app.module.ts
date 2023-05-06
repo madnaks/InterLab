9c2d5626a2b7d60cf294c261edaaf7f3309dd7d1
MADNAKS\Skander <med.haouam@gmail.com>
2023-05-05 23:08:02 -0400

    #1 - Initial client project

--- InterLab-Client/src/app/app.module.ts
+++ InterLab-Client/src/app/app.module.ts
@@ -1,8 +1,26 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr);
+
+const AngularMaterialModules = [
+  MatCardModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatIconModule
+];
 
 @NgModule({
   declarations: [
@@ -10,9 +28,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularMaterialModules
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
