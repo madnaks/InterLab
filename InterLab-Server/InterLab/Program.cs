bd14799e7487721c8d92e2e4c09d0739776b01d8
MADNAKS\Skander <med.haouam@gmail.com>
2023-05-05 23:06:25 -0400

    #1 - Accept localhost origin in dev env

--- InterLab-Server/InterLab/Program.cs
+++ InterLab-Server/InterLab/Program.cs
@@ -16,6 +16,16 @@ builder.Services.AddScoped<IStockDataService, StockDataService>();
 // Register your custom typed HttpClient
 builder.Services.AddHttpClient<IStockDataService, StockDataService>();
 
+// Add new policy for localhost origin
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "localhostOrigin",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200");
+                      });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +33,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    // add localhost origin in development environment
+    app.UseCors("localhostOrigin");
 }
 
 app.MapWhen(context => context.Request.Path.StartsWithSegments("/api.goperigon.com"), appBuilder => {
