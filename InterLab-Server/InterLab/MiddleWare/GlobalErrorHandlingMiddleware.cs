3592f684b2b9af858a884e5c04d68c9131fad97b
MADNAKS\Skander <med.haouam@gmail.com>
2023-05-25 17:16:56 -0400

    Add exception handling

---
+++ InterLab-Server/InterLab/MiddleWare/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using InterLab.Core.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace InterLab.MiddleWare
+{
+    public class GlobalErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var exceptionResult = JsonSerializer.Serialize(new { errorMessage = exception.Message, exception.StackTrace });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)GetExceptionStatusCode(exception);
+
+            return context.Response.WriteAsync(exceptionResult);
+        }
+
+        private static HttpStatusCode GetExceptionStatusCode(Exception exception)
+        {
+            var statusMappings = GetStatusMappings();
+
+            var exceptionType = exception.GetType();
+            return statusMappings.TryGetValue(exceptionType, out var statusCode)
+                ? statusCode
+                : HttpStatusCode.InternalServerError;
+        }
+
+        private static Dictionary<Type, HttpStatusCode> GetStatusMappings()
+        {
+            return new Dictionary<Type, HttpStatusCode>
+            {
+                { typeof(BadRequestException), HttpStatusCode.BadRequest },
+                { typeof(NotFoundException), HttpStatusCode.NotFound },
+                { typeof(NotImplementedException), HttpStatusCode.NotImplemented },
+                { typeof(UnauthorizedAccessException), HttpStatusCode.Unauthorized },
+                { typeof(KeyNotFoundException), HttpStatusCode.Unauthorized },
+            };
+        }
+    }
+}
