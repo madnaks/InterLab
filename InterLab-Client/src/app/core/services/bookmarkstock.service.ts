cedd4ef2ee155b62d3fe5ff1ab477b916c71b003
MADNAKS\Skander <med.haouam@gmail.com>
2023-07-23 23:04:13 -0400

    Add environment files

--- InterLab-Client/src/app/core/services/bookmarkstock.service.ts
+++ InterLab-Client/src/app/core/services/bookmarkstock.service.ts
@@ -2,21 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Stock } from '../models/stock';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookMarkStockService {
 
-  readonly rootURL = 'https://localhost:44303/BookMarkStock/';
-
   constructor(private http: HttpClient) { }
 
   saveBookMarkStock(stock: Stock): Observable<boolean> {
-    return this.http.post<boolean>(this.rootURL + 'SaveBookMarkStock', stock);
+    return this.http.post<boolean>(environment.bookMarkStockUrl + 'SaveBookMarkStock', stock);
   }
 
   getBookmarkStocks(symbol: string, date: string | null): Observable<Stock[]> {
-    return this.http.get<Stock[]>(this.rootURL + 'GetBookmarkStocks/'+ symbol + '/' + date);
+    return this.http.get<Stock[]>(environment.bookMarkStockUrl + 'GetBookmarkStocks/'+ symbol + '/' + date);
   }
 }
---
+++ InterLab-Client/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  stockUrl: 'https://localhost:44303/Stock/',
+  bookMarkStockUrl: 'https://localhost:44303/BookMarkStock/',
+};
