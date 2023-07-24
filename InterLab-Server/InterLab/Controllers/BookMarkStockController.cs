96663679a557a024f1438b91036ca341df8ba1f6
MADNAKS\Skander <med.haouam@gmail.com>
2023-07-23 23:04:24 -0400

    clean project

--- InterLab-Server/InterLab/Controllers/BookMarkStockController.cs
+++ InterLab-Server/InterLab/Controllers/BookMarkStockController.cs
@@ -4,8 +4,6 @@ using InterLab.Core.Dto;
 using InterLab.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace InterLab.Controllers
 {
     [Route("[controller]")]
